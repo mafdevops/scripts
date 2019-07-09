78545cdada547ddbd6a3b0e3d730ebaaa032abee
mafdevops <52715555+mafdevops@users.noreply.github.com>
2019-07-09 22:32:20 +0400

    Update and rename system-resources.sh to system-resources-ubuntu.sh

---
+++ system-resources-ubuntu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt update
+
+# Checking free system memory
+vmstat -s
+
+# Checking free disk space
+df -H
+
+# Checking CPU usage
+sudo apt install -y sysstat
+iostat
+
+# Count number of lines in a file: create a new file, add contents to it from a URL, and count the total number of lines: 
+touch numberoflinex.txt
+wget "https://ubuntu.com/about" -O numberoflines.txt
+wc –l numberoflines.txt
+
+
+# Open TCP port: Let's install Apache web server, stop the service, check the opened ports, then open the port and check it
+    # Install Apache
+sudo apt install -y apache2
+sudo ufw app list
+sudo ufw allow 'Apache'
+
+    # Check its port, it shoild be listening to 80
+netstat -tulpn
+    # Stop Apache service
+sudo systemctl stop apache2
+    # Check its portagain, it shouldn't be listed
+netstat -tulpn
+    # Do the same to re-open the port by starting the service
+sudo systemctl start apache2 && netstat -tulpn
