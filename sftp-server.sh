1e2a84daa3a57c13dd3165d7671a9aca85c1bd68
mafdevops <52715555+mafdevops@users.noreply.github.com>
2019-07-10 22:10:47 +0400

    Update sftp-server.sh

--- sftp-server.sh
+++ sftp-server.sh
@@ -8,12 +8,17 @@ sudo apt install openssh-server
 sudo systemctl enable ssh.service
 sudo systemctl start ssh.service
 
-
+# Configure sftp and restart ssh service
 sudo nano /etc/ssh/sshd_config
 sudo systemctl restart ssh.service
 
-
+# create new group for sftp called sftp_users
 sudo groupadd sftp_users
 
+# create new user called backup, add it to the group, and set his password
 sudo usermod -aG sftp_users backup
 passwd backup
+
+# giev the grou pwrite permission to user home directory 
+sudo chgrp -R sftp_users /home/ubuntu/
+sudo chmod -R g+w /home/ubuntu/
