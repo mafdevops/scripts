a0a4b1f2ed8e9d132c2405d3449f19d308c5e86d
mafdevops <52715555+mafdevops@users.noreply.github.com>
2019-07-10 21:36:02 +0400

    Update string-occurrences.sh

--- string-occurrences.sh
+++ string-occurrences.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+sudo apt update -y
 
-touch numberoflines.txt
-wget "https://ubuntu.com/about" -O numberoflines.txt
+# create a new file from the source of about AWS page 
+curl "https://aws.amazon.com/about-aws/" > stringnumber.txt
+
+# search for AWS string in the file
+grep -o -i aws stringnumber.txt | wc -l
+
+# Now, let's count the word: service, the output will include services as well
+# for example, the command will grep "service" as a word  and amazon web "service"s as it's part of the line.
+grep -o -i service stringnumber.txt | wc -l
+
+# To count the whole word
+grep -o '\<service\>' stringnumber.txt | wc -l
