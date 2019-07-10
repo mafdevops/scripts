61304f3243a1f3ea97ce8561bb34fec2789b68e3
mafdevops <52715555+mafdevops@users.noreply.github.com>
2019-07-10 21:55:30 +0400

    Update sftp-server.sh

--- sftp-server.sh
+++ sftp-server.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+sudo apt update -y
 
+# Instal OpenSSH
+sudo apt install openssh-server
+
+# Enable and start SSH
+sudo systemctl enable ssh.service
+sudo systemctl start ssh.service
+
+
+sudo nano /etc/ssh/sshd_config
+sudo systemctl restart ssh.service
+
+
+sudo groupadd sftp_users
+
+sudo usermod -aG sftp_users backup
+passwd backup
